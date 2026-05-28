cad23f7d5066e09cda46da6cd4fcae889912046c
Thai Duong <duongevil123@gmail.com>
2026-05-28 22:29:46 +0700

    machinery

--- backend/src/upload/upload.module.ts
+++ backend/src/upload/upload.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
+import { User, UserSchema } from '../users/schemas/user.schema';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
